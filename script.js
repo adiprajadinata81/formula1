d295251a98bb31b86dc934c21ef0f62e889de679
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-09-13 18:41:19 +0700

    Modified and add file

---
+++ script.js
@@ -0,0 +1,20 @@
+const sidebar = document.getElementById('sidebar');
+const menuToggle = document.getElementById('menu-toggle');
+const closeBtn = document.getElementById('close-btn');
+const overlay = document.getElementById('overlay');
+
+menuToggle.addEventListener('click', () => {
+  sidebar.classList.add('active');
+  overlay.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.remove('active');
+  overlay.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
+  sidebar.classList.remove('active');
+  overlay.classList.remove('active');
+});
+
